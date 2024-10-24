25459e34b962adf1d8c3aef760a1b5b20f69391e
Georgii Moroz <63062381+uthbq@users.noreply.github.com>
2024-10-24 18:58:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<<?php 
+// Подключение файла соединения с БД
+include_once 'db.class.php';
+
+// хост БД
+define('db_host','localhost');
+
+// Имя БД
+define('db_name','base');
+
+// Пользователь БД
+define('db_user','admin');
+
+// Пароль БД
+define('db_pass','admin_password');
+
+// Обявление класса для подключения к бд
+$db = new DB_class(db_host,db_name,db_user,db_pass);
+
+$query = $db->select(true,"*","base","task_id='1'");
+
+print_r($query);
+ ?>
